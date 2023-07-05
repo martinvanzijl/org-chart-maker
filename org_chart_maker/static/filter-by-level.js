fb34b3e14242c264f16a3b44274b55c54e039d29
Martin van Zijl <martin.vanzijl@gmail.com>
2023-07-05 14:04:16 +1200

    Remove C++ comments.

--- org_chart_maker/static/filter-by-level.js
+++ org_chart_maker/static/filter-by-level.js
@@ -38,26 +38,21 @@ function getChildren(person) {
 }
 
 // A temporary map to store the depth of each person.
-// static QMap<DiagramItem *, int> m_depthMap; // C++
 var m_depthMap = {};
 
 //
 // Get the "depth" of the person, i.e. how many layers of descendants they have.
 //
-// static int getDepth(DiagramItem *person) { // C++
 function getDepth(person) {
     // Check if depth already calculated.
-    // if (m_depthMap.contains(person)) { // C++
     if (person in m_depthMap) {
         return m_depthMap[person.personId];
     }
 
     // Get maximum depth of parents.
-    // QList<DiagramItem *> children = person->getChildren(); // C++
     var children = getChildren(person);
 
     var maxChildDepth = -1;
-    // for (DiagramItem *child: children) { // C++
     for (var index in children) {
         var child = children[index];
         var childDepth = getDepth(child);
@@ -77,7 +72,6 @@ function getDepth(person) {
 }
 
 // Automatically lay out the given row of persons at the given Y coordinate.
-// int DiagramScene::autoLayoutRow(const QList<DiagramItem *> &items, int startY) { // C++
 function autoLayoutRow(items, startY) {
     var startX = 128;
 
@@ -88,24 +82,18 @@ function autoLayoutRow(items, startY) {
     var verticalSpacing = 16;
 
     // Lay out each item in the row.
-    // for (auto item: items) { // C++
     for (var index in items) {
         // Get the person.
         var person = items[index];
-        // DiagramItem *person = qgraphicsitem_cast<DiagramItem*> (item); // C++
 
         // Place the person.
-        // person->setPos(x, y); // C++
         setPersonPos(person, x, y);
 
         // Prepare the next position.
-        // x += person->getWidthIncludingSpouse() + horizontalSpacing; // C++
         x += getPersonWidth(person) + horizontalSpacing;
 
         // Wrap to next line if required.
-        // if (x > sceneRect().right()) {
         if (x > getDiagramWidth()) {
-            // y += person->boundingRect().height() + verticalSpacing;
             y += person.getHeight() + verticalSpacing;
             x = startX;
         }
@@ -118,38 +106,28 @@ function autoLayoutRow(items, startY) {
 function autoLayout()
 {
     // Clear depth map.
-    // m_depthMap.clear(); // C++
     m_depthMap = {};
 
     // Set up map of depth to persons.
-    // QMultiMap<int, DiagramItem* > depthMultiMap; // C++
     var depthMultiMap = {};
 
     // Find the right depth for each person.
-    // for (auto item: items()) { // C++
     for (var index in persons) {
         var person = persons[index];
-        // if (item->type() == DiagramItem::Type) { // C++
-            // DiagramItem *person = qgraphicsitem_cast<DiagramItem*> (item); // C++
-
-            var depth = getDepth(person);
-            if (!(depth in depthMultiMap)) {
-              depthMultiMap[depth] = [];
-            }
-            // depthMultiMap.insert(depth, person); // C++
-            depthMultiMap[depth].push(person);
-        // } // C++
+        var depth = getDepth(person);
+        if (!(depth in depthMultiMap)) {
+            depthMultiMap[depth] = [];
+        }
+        depthMultiMap[depth].push(person);
     }
 
     // Get maximum depth.
-    // auto maxDepth = depthMultiMap.lastKey(); // C++
     var maxDepth = getLastKey(depthMultiMap);
 
     // Place the persons.
     var y = 64;
 
     for (var depth = maxDepth; depth >= 0; --depth) {
-        // y = autoLayoutRow(depthMultiMap.values(depth), y); // C++
         y = autoLayoutRow(depthMultiMap[depth], y);
         // y += 256; // Too much.
         // y += 128; // Too little.
