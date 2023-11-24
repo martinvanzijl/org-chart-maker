a9fbdadeadf27f3118fbbc8301f723e3b1336c7c
Martin van Zijl <martin.vanzijl@gmail.com>
2023-11-24 17:12:15 +1300

    Add function to import selected CSV file.

---
+++ org_chart_maker/static/csv-import.js
@@ -0,0 +1,9 @@
+// -----------------------------------------------------------------------------
+// Routines for importing CSV files.
+// -----------------------------------------------------------------------------
+
+// Import the selected CSV file.
+function importSelectedCsvFile() {
+  // Debug.
+  console.log("Importing selected CSV file...");
+}
