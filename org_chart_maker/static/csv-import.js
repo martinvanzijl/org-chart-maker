fb53b53a7e355cd3eb51783d7591dfc762107cee
Martin van Zijl <martin.vanzijl@gmail.com>
2023-12-15 17:18:48 +1300

    Update help file.

--- org_chart_maker/static/csv-import.js
+++ org_chart_maker/static/csv-import.js
@@ -2,19 +2,6 @@
 // Routines for importing CSV files.
 // -----------------------------------------------------------------------------
 
-// Import the selected CSV file.
-// function importSelectedCsvFile() {
-//   // Debug.
-//   console.log("Importing selected CSV file...");
-//
-//   // Get fields.
-//   var fileName = document.getElementById("import_csv_input").value;
-//   console.log("File name:", fileName);
-//
-//   // TODO:
-//   // Set form target to Python method: import_csv_file.
-// }
-
 // Add CSV data to the diagram.
 function addCsvDataToDiagram (data) {
 
